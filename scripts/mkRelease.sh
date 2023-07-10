e69ec6d8cad951582e2b884e8f53afe1617c0b7e
David Mohammed <fossfreedom@ubuntu.com>
2023-07-10 20:43:57 +0100

    v0.9.2 version bump

--- scripts/mkRelease.sh
+++ scripts/mkRelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="0.9.1"
+VERSION="0.9.2"
 NAME="magpie"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
