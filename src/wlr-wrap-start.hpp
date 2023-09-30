f5b9179d134103ed41022dbb94e636b61ff8af51
Campbell Jones <git@serebit.com>
2023-09-30 10:17:05 -0400

    Fix Wkeyword-macro warnings produced by clang

--- src/wlr-wrap-start.hpp
+++ src/wlr-wrap-start.hpp
@@ -6,9 +6,13 @@ static_assert(0 == 1, "wlroots include wrap started and not ended");
 
 #include <pthread.h>
 #include <wayland-server-core.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wkeyword-macro"
 #define class _class
 #define namespace _namespace
 #define static
+#pragma clang diagnostic pop
 #define WLR_USE_UNSTABLE
 
 extern "C" {
